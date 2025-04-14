5797697158a5ce106a7eed6666b2ba46de320641
desabi <desabi@live.com>
2025-04-14 17:46:50 -0600

    feat: leetcode-easy, problem 217

---
+++ leetcode-easy/src/main/java/com/desabisc/leetcode/easy/arrayhashing/p217/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+package com.desabisc.leetcode.easy.arrayhashing.p217;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+  public static void main(String[] args) {
+    int[] numbers = new int[] {1,2,3,1};
+    System.out.println(solution1(numbers));;
+  }
+
+  static boolean solution1(int[] numbers) {
+    HashSet<Integer> duplicates = new HashSet<>();
+
+    for (int currentNumber : numbers) {
+      if (duplicates.contains(currentNumber)) {
+        return true;
+      }
+
+      duplicates.add(currentNumber);
+    }
+    return false;
+  }
+}
